897aabb20baa99186f56f6fe32a68e862956cc78
Corner <p_wq@outlook.com>
2018-04-24 15:32:54 +0800

    pwq update

--- netVis.py
+++ netVis.py
@@ -6,33 +6,9 @@ from flask import Flask, request
 from flask import render_template, jsonify
 from igraph import *
 
-all_files_data = []
-layout_data = {}
-time_data = {}
 upload_file_index = 0
 
 app = Flask(__name__)
-
-
-def read_packages():
-    path = 'files/jsonFormat/packages/'
-    files = os.listdir(path)
-    for f in files:
-        json_data = json.load(open(path + f))
-        f = f.replace('.json', '')
-        f = f.replace('_', ':')
-        item = {'time': f, 'data': json_data}
-        all_files_data.append(item)
-
-    file_data = json.load(open('files/jsonFormat/time-line.json'))
-    time_data['packages'] = file_data['packages']
-
-    file_data = json.load(open('files/jsonFormat/small-443nodes-476edges.json'))
-    layout_data['nodes'] = file_data['nodes']
-    layout_data['links'] = file_data['links']
-
-
-read_packages()
 
 
 def cal_back_layout_data(result, layout_type):
@@ -76,16 +52,19 @@ def index():
 
 @app.route('/initial')
 def get_initial_data():
-    return jsonify(time_data)
+    with open('files/jsonFormat/time-line.json') as fi:
+        result = json.load(fi)
+        return jsonify(result)
 
 
 @app.route('/layout')
 def get_back_layout_data():
     layout_type = request.args.get('layout_type')
-    result = copy.deepcopy(layout_data)
-    cal_back_layout_data(result, layout_type)
-    calNetwork.cal_characters_arguments(result)
-    return jsonify(result)
+    with open('files/jsonFormat/small-443nodes-476edges.json') as fi:
+        result = json.load(fi)
+        cal_back_layout_data(result, layout_type)
+        calNetwork.cal_characters_arguments(result)
+        return jsonify(result)
 
 
 @app.route('/brush_extent')
@@ -100,31 +79,37 @@ def get_brush_extent_data():
     start_time = request.args.get('start')
     end_time = request.args.get('end')
     layout_type = request.args.get('layout_type')
-    for item in all_files_data:
-        if item['time'] == start_time:
+    path = 'files/jsonFormat/packages/'
+    files = os.listdir(path)
+    for f in files:
+        time = f.replace('.json', '')
+        time = time.replace('_', ':')
+        if time == start_time:
             flag = not flag
-        if item['time'] == end_time:
+        if time == end_time:
             flag = not flag
         if flag:
-            for node in item['data']['nodes']:
-                if node['id'] not in nodes:
-                    result['nodes'].append(node)
-                    nodes.append(node['id'])
-                else:
-                    for re_node in result['nodes']:
-                        if re_node['id'] == node['id']:
-                            result['nodes'].remove(re_node)
-                            result['nodes'].append(node)
-                            break
-            for link in item['data']['links']:
-                if link['id'] not in links:
-                    result['links'].append(link)
-                    links.append(link['id'])
-                else:
-                    for re_link in result['links']:
-                        if re_link['id'] == link['id']:
-                            result['links'].remove(re_link)
-                            result['links'].append(link)
+            with open(path + f) as fi:
+                json_data = json.load(fi)
+                for node in json_data['nodes']:
+                    if node['id'] not in nodes:
+                        result['nodes'].append(node)
+                        nodes.append(node['id'])
+                    else:
+                        for re_node in result['nodes']:
+                            if re_node['id'] == node['id']:
+                                result['nodes'].remove(re_node)
+                                result['nodes'].append(node)
+                                break
+                for link in json_data['links']:
+                    if link['id'] not in links:
+                        result['links'].append(link)
+                        links.append(link['id'])
+                    else:
+                        for re_link in result['links']:
+                            if re_link['id'] == link['id']:
+                                result['links'].remove(re_link)
+                                result['links'].append(link)
     cal_back_layout_data(result, layout_type)
     calNetwork.cal_characters_arguments(result)
     return jsonify(result)
@@ -148,14 +133,11 @@ def up_load_file():
 def up_load_file_layout():
     layout_type = request.args.get('layout_type')
     file_path = request.args.get('file_path')
-    result = {}
-    file_data = json.load(open(file_path))
-    result['nodes'] = file_data['nodes']
-    result['links'] = file_data['links']
-
-    cal_back_layout_data(result, layout_type)
-    calNetwork.cal_characters_arguments(result)
-    return jsonify(result)
+    with open(file_path) as fi:
+        result = json.load(fi)
+        cal_back_layout_data(result, layout_type)
+        calNetwork.cal_characters_arguments(result)
+        return jsonify(result)
 
 
 if __name__ == '__main__':
